42647c5f1f53d924dc62f7941f35e0fdd894055c
rohitshakya207 <rohitshakya207@gmail.com>
2016-11-08 19:02:49 +0530

    initial commit

---
+++ app/src/androidTest/java/contlist/com/org/myfirstproject/ApplicationTest.java
@@ -0,0 +1,13 @@
+package contlist.com.org.myfirstproject;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
